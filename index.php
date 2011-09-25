c16df019c2222487a0658274681706a74ede96b7
Simon Lepel <simbo@simbo.de>
2011-09-25 16:28:19 +0200

    a lot of improvements...
    Signed-off-by: Simon Lepel <simbo@simbo.de>

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
 
-	$maxNumberOfImages = 35;
+	$maxNumberOfImages = 36;
 	
-	$cols = 0;
-	$rows = 0;
-	$numberOfImages = 0;
 	$images = array();
 	
-	if( isset($_GET['i']) && isset($_GET['c']) && isset($_GET['r']) ) {
-		$numberOfImages = floor(intval($_GET['i'])/2);
-		$cols = intval($_GET['c']);
-		$rows = intval($_GET['r']);
+	if( isset($_GET['i']) ) {
+		$i = min( $maxNumberOfImages*2, max( 12, intval($_GET['i']) ) );
+		$numberOfImages = floor($i/2);
+		$cols = ceil(sqrt($i));
+		$rows = ceil($i/$cols);
 		while( count($images)<$numberOfImages ) {
 			$rand = rand(1,$maxNumberOfImages);
 			if( !in_array($rand,$images) )
@@ -22,17 +20,17 @@
 
 echo '<!doctype html>'
 	.'<html>'
-	.'<head>'
-	.'<meta charset="utf-8">'
-	.'<title>Memory</title>'
-	.'<meta name="author" content="Simon Lepel">'
-	.'<link rel="stylesheet" href="css/memory.css" />'
-	.'<link rel="stylesheet" href="css/ui-lightness/jquery-ui.css" />'
-	.'<script src="js/jquery.js"></script>'
-	.'<script src="js/jquery-ui.js"></script>'
-	.'<script src="js/memory.js"></script>'
-	.'</head>'
-	.'<body>';
+		.'<head>'
+			.'<meta charset="utf-8">'
+			.'<title>Memory</title>'
+			.'<meta name="author" content="Simon Lepel">'
+			.'<link rel="stylesheet" href="css/memory.css" />'
+			.'<link rel="stylesheet" href="css/ui-lightness/jquery-ui.css" />'
+			.'<script src="js/jquery.js"></script>'
+			.'<script src="js/jquery-ui.js"></script>'
+			.'<script src="js/memory.js"></script>'
+		.'</head>'
+		.'<body>';
 
 if( !empty($images) ) {
 	echo '<div id="memory">';
@@ -45,35 +43,32 @@ if( !empty($images) ) {
 		}
 		echo '</ul>';
 	}
-	echo '<br class="clear" />'
-		.'<p>Pairs found: <span id="s">0</span> of '.$numberOfImages.'</p>'
-		.'<p>Attempts: <span id="a">0</span></p>'
-		.'<p>Time elapsed: <span id="t">0</span></p>'
+	echo 	'<br class="clear" />'
+			.'<p>Pairs found: <span id="s">0</span> of '.$numberOfImages.'</p>'
+			.'<p>Attempts: <span id="a">0</span></p>'
+			.'<p>Time elapsed: <span id="t">0</span></p>'
 		.'</div>';
 }
 
 else {
 	echo '<form id="memory" action="." method="get">'
-		.'<fieldset>'
-		.'<strong>Play Memory</strong>'
-		.'<input type="hidden" id="i" name="i" value="36" />'
-		.'<input type="hidden" id="c" name="c" value="6" />'
-		.'<input type="hidden" id="r" name="r" value="6" />'
-		.'<p>'
-		.'<label for="i">Cards:</label>'
-		.'<span id="is">36</span>'
-		.'<div id="iSlider"></div>'
-		.'</p><p>'
-		.'<label for="c">Cols:</label>'
-		.'<span id="cs">6</span>'
-		.'</p><p>'
-		.'<label for="r">Rows:</label>'
-		.'<span id="rs">6</span>'
-		.'</p><p class="center">'
-		.'<input class="ui-button" type="submit" value="Start game" />'
-		.'<small><a href="https://github.com/simbo/Memory-Game">view source at github</a></small>'
-		.'</p>'
-		.'</fieldset>'
+			.'<fieldset>'
+				.'<strong>Play Memory</strong>'
+				.'<p>'
+					.'<label for="i">Cards:</label>'
+					.'<input type="text" id="i" name="i" value="" />'
+					.'<div id="iSlider"></div>'
+				.'</p><p>'
+					.'<label for="c">Cols:</label>'
+					.'<span id="c"></span>'
+				.'</p><p>'
+					.'<label for="r">Rows:</label>'
+					.'<span id="r"></span>'
+				.'</p><p class="center">'
+					.'<input class="ui-button" type="submit" value="Start game" />'
+					.'<small><a href="https://github.com/simbo/Memory-Game">view source at github</a></small>'
+				.'</p>'
+			.'</fieldset>'
 		.'</form>';
 }
 
--- js/memory.js
+++ js/memory.js
@@ -1,186 +1,194 @@
 
-// initial options
-var initial = {
-	i: 36,
-	c: 6,
-	r: 6
-};
+// the game...
+var memory = {
+
+	// memory items
+	items: null,
+	
+	// text elements (solved, attempts, time)
+	s: null, a: null, t: null,
+	
+	// timer reference for hiding items automatically
+	hideTimer: null,
+	// timer reference for elapsed time
+	timer: null,
+	// start time for elapsed time
+	startTime: 0,
+	
+	// initialize
+	init: function() {
+	
+		// set items
+		this.items = $('div#memory li');
+		
+		// set text elements
+		this.s = $('#s');
+		this.a = $('#a');
+		this.t = $('#t');
+		
+		// set game container width relative to column count
+		$('div#memory').width( $('div#memory ul:first li').length*109 );
+	
+		// set start time
+		this.startTime = new Date().getTime();
+		// set interval to update timer
+		this.timer = window.setInterval(function(){
+			// time difference
+			var d = Math.round( ((new Date().getTime())-memory.startTime)/1000 ),
+			// timer text
+				t = '';
+			// hours
+			d>=3600 && (t+=Math.floor(d/3600)+'h ') && (d%=3600)>=0;
+			// minutes
+			d>=60 && (t+=Math.floor(d/60)+'m ') && (d%=60)>=0;
+			// seconds
+			t += d+'s';
+			// set timer text
+			memory.t.text(t);
+		},1000);
+		
+		// each item
+		this.items.each(function(i){
+			// set click event
+			$(this).click(function(){
+				// if not solved
+				if( !$(this).hasClass('solved') ) {
+					// get visible items
+					var v=memory.getVisible();
+					// if 2 or more items visible
+					if( v.length>=2 ) {
+						// hide them and set next item to show
+						memory.hideItem( v, $(this) )
+						// clear hideTimer
+						clearTimeout( memory.hideTimer );
+					}
+					else
+						// show item
+						memory.showItem( $(this) );
+				}
+			});
+		});
+	},
+	
+	// get visible items
+	getVisible: function() {
+		return this.items.filter('.visible');
+	},
+	
+	// show item
+	showItem: function( item ) {
+		// add class 'visible' to item, find img, fade in
+		item.addClass('visible').find('img').fadeIn(200,function(){
+			// get visible items
+			var v = memory.getVisible();
+			// if 2 items visible
+			if( v.length==2 ) {
+				// if both have same img
+				if( v.filter(':eq(0)').find('img').get(0).src == v.filter(':eq(1)').find('img').get(0).src ) {
+					// set solved text
+					memory.s.text( parseInt($('#s').text())+1 );
+					// add class 'solved' to items and fade out
+					v.addClass('solved').animate({ opacity:0 },800);
+					// if all items are solved
+					if( memory.items.length == memory.items.filter('.solved').length ) {
+						// stop timer
+						window.clearInterval(memory.timer);
+						// hide game
+						$('div#memory').fadeOut(200);
+						// show winning screen
+						$('<div id="solved"><strong>Congratulations!</strong>'
+							+'<p>You found '+memory.s.text()+' pairs with '+memory.a.text()+' attempts in '+memory.t.text()+'.</p>'
+							+'<p><a href=".">Play again&hellip;</a></p></div>').hide().appendTo('body').fadeIn(200);
+						// trigger window.resize for positioning
+						$(window).resize();
+					}
+				}
+				// if item images are different
+				else
+					// hide visible items after 2 seconds
+					memory.hideTimer = window.setTimeout(function(){
+						memory.hideItem(v);
+					},2000);
+			}							
+		});
+		// if 2 items visible
+		if( this.getVisible().length==2 )
+			// set attempts text
+			this.a.text( parseInt(this.a.text())+1 );
+	},
+	
+	// hide item
+	hideItem: function( item, next ) {
+		// remove class 'visible' from item, find img, fade out
+		item.removeClass('visible').find('img').fadeOut(200,function(){
+			// if next item set
+			if( next!=undefined )
+				// show next item
+				memory.showItem(next);
+		});
+	}
+}
+
+// options handler
+var options = {
+
+	// element references
+	i:null, c: null, r: null,
+	
+	// initialize
+	init: function() {
+		// set elements
+		this.i = $('#i');
+		this.c = $('#c');
+		this.r = $('#r');
+		// set default values
+		this.set(36);
+	},
+
+	// set values
+	set: function(i) {
+		// calculate cols and rows
+		var c = Math.ceil(Math.sqrt(i));
+		this.i.val(i);
+		this.c.text(c);
+		this.r.text( Math.ceil(i/c) );
+		
+	}
+}
 
 // dom ready event
 $(document).ready(function(){
 
+	// reference for element operations
+	var e = null;
+
 	// window resize event
 	$(window).resize(function(){
-		// center options form and winning msg
-		if( $('form#memory,div#solved').length>0 )
-			$('form#memory,div#solved').position({
-				of: $(window),
-				my: 'center center',
-				at: 'center center',
-				offset: '0 -'+($(window).height()/10)
-			});
-		// center game
-		if( $('div#memory').length>0 )
-			$('div#memory').position({
-				of: $(window),
-				my: 'center top',
-				at: 'center top',
-				offset: '0 10'
-			});
+		// center options form and/or winning msg
+		(e=$('form#memory,div#solved')) && e.length>0 && e.position({
+			of: $(window),
+			my: 'center center',
+			at: 'center center',
+			offset: '0 -'+($(window).height()/10)
+		});
 	});
+
 	// trigger resize
 	$(window).resize();
 
-	// options form
-	if( $('form#memory').length>0 ) {
-		// slider
-		$('#iSlider').slider({
-			min: 4,
-			max: 70,
-			value: 36,
-			step: 2,
-			slide: function(ev,ui) {
-				// calculate cols and rows
-				var i = ui.value,
-					c = Math.ceil(Math.sqrt(i)),
-					r = Math.ceil(i/c);
-				// set options
-				setValues(i,c,r);
-			}
-		});
-		// set initial options
-		setValues(initial.i,initial.c,initial.r);
-	}
-	
-	// set options
-	function setValues( i, c, r ) {
-		$('#is').text(i);
-		$('#i').val(i);
-		$('#cs').text(c);
-		$('#c').val(c);
-		$('#rs').text(r);
-		$('#r').val(r);
-	}
-	
-	
-	if( $('div#memory').length>0 ) {
-	
-		// the game...
-		var memory = {
-			
-			// memory items
-			items: $('div#memory li'),
-			
-			// timer references
-			hideTimer: null,
-			timer: null,
-			startTime: 0,
-			
-			// initialize
-			init: function() {
-				var m = this;
-				
-				// timer
-				this.startTime = new Date().getTime();
-				this.timer = window.setInterval(function(){
-					var d = Math.round( (( new Date().getTime() ) - m.startTime )/1000 ),
-						t = '';
-					// hours
-					if( d>3600 ) {
-						t += Math.floor(d/3600)+'h ';
-						d = d%3600;
-					}
-					// minutes
-					if( d>60 ) {
-						t += Math.floor(d/60)+'m ';
-						d = d%60;
-					}
-					// seconds
-					t += d+'s';
-					// set time text
-					$('#t').text(t);
-				},1000);
-				
-				// click events
-				this.items.each(function(i){
-					$(this).click(function(){
-						if( !$(this).hasClass('solved') ) {
-							var v = m.getVisibles();
-							// if 2 or more items visible
-							if( v.length>=2 ) {
-								// hide them
-								m.hideItem(v,$(this))
-								// clear hideTimer
-								clearTimeout(m.hideTimer);
-							}
-							else
-								m.showItem($(this));
-						}
-					});
-				});
-			},
-			
-			// get visible items
-			getVisibles: function() {
-				return this.items.filter('.visible');
-			},
-			
-			// show item
-			showItem: function( item ) {
-				var m = this;
-				item.addClass('visible').find('img').fadeIn(200,function(){
-					var v = m.getVisibles();
-					// if 2 items visible
-					if( v.length==2 ) {
-						// if both have same img
-						if( v.filter(':eq(0)').find('img').get(0).src == v.filter(':eq(1)').find('img').get(0).src ) {
-							// set solved text
-							$('#s').text( parseInt($('#s').text())+1 );
-							// fade out solved items
-							v.addClass('solved').animate({ opacity:0 },800);
-							// if all items solved
-							if( m.items.length == m.items.filter('.solved').length ) {
-								// stop timer
-								window.clearInterval(m.timer);
-								// hide game
-								$('div#memory').fadeOut(200);
-								// show winning screen
-								$('<div id="solved"><strong>Congratulations!</strong>'
-									+'<p>You found '+$('#s').text()+' pairs with '+$('#a').text()+' attempts in '+$('#t').text()+'.</p>'
-									+'<p><a href=".">Play again&hellip;</a></p></div>').hide().appendTo('body').fadeIn(200);
-								// trigger window.resize for positioning
-								$(window).resize();
-							}
-						}
-						// if item images are different
-						else
-							// hide visible items after 2 seconds
-							m.hideTimer = window.setTimeout(function(){
-								m.hideItem(v);
-							},2000);
-					}							
-				});
-				// if 2 items visible
-				if( this.getVisibles().length==2 )
-					// set attempts text
-					$('#a').text( parseInt($('#a').text())+1 );
-			},
-			
-			// hide item
-			hideItem: function( item, next ) {
-				var m = this;
-				item.removeClass('visible').find('img').fadeOut(200,function(){
-					// if next item set
-					if( next!=undefined )
-						// show next item
-						m.showItem(next);
-				});
-			}
-			
+	// if slider element present, add slider and initialize options
+	(e=$('#iSlider')) && e.length>0 && e.slider({
+		min: 12,
+		max: 72,
+		value: 36,
+		step: 2,
+		slide: function(ev,ui) {
+			options.set(ui.value);
 		}
-		// initialize
-		memory.init();		
-		
-	}
+	// initialize options
+	}) && options.init();
+	
+	// if game container present, intialize game
+	$('div#memory').length>0 && memory.init();
 
 });
